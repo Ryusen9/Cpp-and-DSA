fd777e17e66aa6b9c05e457ab223b1c70179e728
Ryusen9 <asratul02@gmail.com>
2025-01-03 01:37:28 +0600

    Initial commit

---
+++ Day 1/DatatypeSize.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3;
+    char b = 'a';
+    bool c = false;
+    float d = 3.14;
+    double e = 2.71828;
+    cout << "size of int = "<< sizeof(int)<<endl;
+    cout << "size of char = "<< sizeof(char)<<endl;
+    cout << "size of bool = "<< sizeof(bool)<<endl;
+    cout << "size of float = "<< sizeof(float)<<endl;
+    cout << "size of double = "<< sizeof(double)<<endl;
+    return 0;
+}
---
+++ Day 1/Pattern.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // std::cout << "****\n";
+    // std::cout << "***\n";
+    // std::cout << "**\n";
+    // std::cout << "*";
+    std::cout << "****\n***\n**\n*";
+    return 0;
+}
